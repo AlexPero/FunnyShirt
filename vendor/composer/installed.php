937f4e91e2a2f3eb3192c4e8f4afd63d71d2f2d8
Alexis <a_perotin@digital-campus.net>
2021-06-10 10:29:41 +0200

    ajout gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a2fc70014297986bff1a4ed22d83c163fdb5db00',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a2fc70014297986bff1a4ed22d83c163fdb5db00',
+    ),
+    'htmlburger/carbon-fields' => 
+    array (
+      'pretty_version' => 'v3.3.2',
+      'version' => '3.3.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dd5663e14c6db365323b688dbae1cfbeaf14bee7',
+    ),
+  ),
+);
